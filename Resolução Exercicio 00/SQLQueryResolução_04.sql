fc15852c790c73a87e2cdbdb30896b72e31464ea
Geovanne Barreto <geovannesdi@gmail.com>
2015-04-09 21:50:13 -0300

    Nova resolução exercicio 04 e correção de algumas partes anteriores

---
+++ Resolução Exercicio 00/SQLQueryResolução_04.sql
@@ -0,0 +1,65 @@
+/*EXERCICIO 04, EXERCICIO DE REFORÇO*/
+
+/*
+a.	Selecione as turmas e seus professores.
+*/
+
+SELECT 
+	T.NM_TURMA, P.NM_PROFESSOR
+FROM
+	TB_TURMA T
+INNER JOIN
+	TB_PROFESSOR P ON
+	(T.MATRICULA_PROFESSOR = P.MATRICULA_PROFESSOR) 
+
+/*
+b.	Selecione os alunos matriculados
+e as turmas em que estão matriculados.
+*/
+SELECT 
+	A.NOME, T.NM_TURMA
+FROM 
+   TB_ALUNOS_TURMA AT
+INNER JOIN
+	TB_TURMA T ON
+	(AT.COD_TURMA = T.COD_TURMA)
+INNER JOIN
+	TB_ALUNO A ON
+	(AT.MATRICULA = A.MATRICULA)
+
+/*
+c.	Selecione os alunos matriculados e o nome de seus professores.
+*/
+
+SELECT 
+	A.NOME, P.NM_PROFESSOR
+FROM 
+	TB_ALUNOS_TURMA AT
+INNER JOIN
+	TB_ALUNO A ON
+	(AT.MATRICULA = A.MATRICULA)
+INNER JOIN
+	TB_TURMA T ON
+	(AT.COD_TURMA = T.COD_TURMA)
+INNER JOIN
+	TB_PROFESSOR P ON 
+	(T.MATRICULA_PROFESSOR = P.MATRICULA_PROFESSOR)	
+
+/*
+d.	Selecione todos os professores e, quando existir,
+as turmas nas quais ensinam.
+*/
+
+SELECT 
+	P.NM_PROFESSOR
+FROM
+	TB_PROFESSOR P
+
+
+SELECT 
+	P.NM_PROFESSOR, T.NM_TURMA 
+FROM 
+	TB_TURMA T
+INNER JOIN 
+	TB_PROFESSOR P ON
+	(T.MATRICULA_PROFESSOR = P.MATRICULA_PROFESSOR)
